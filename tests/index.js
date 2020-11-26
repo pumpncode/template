6efefa90e929aaad8607bc0ede870f192dd92af3
nnmrts <nanomiratus@gmail.com>
2020-11-26 07:09:24 +0100

    test: :white_check_mark: update test

--- tests/index.js
+++ tests/index.js
@@ -2,6 +2,6 @@ import test from "ava";
 
 import helloWorld from "../src/index.js";
 
-test("doesn't throw", (t) => {
-	t.notThrows(helloWorld);
+test("returns Hello World!", (t) => {
+	t.is(helloWorld(), "Hello World!");
 });
